1a81344e6633caa8ee23640a5a0127475973768e
Javier Cuadros <ing.javiercuadros@gmail.com>
2024-02-27 18:19:54 -0500

    Proyecto catalogo peliculas en java

---
+++ CatalogoPeliculas/src/mx/com/gm/peliculas/exepciones/AccesoDatosEx.java
@@ -0,0 +1,12 @@
+package mx.com.gm.peliculas.exepciones;
+
+
+public class AccesoDatosEx extends Exception{
+    
+    public AccesoDatosEx(String mensaje){
+        
+        super(mensaje);
+        
+    }
+    
+}
